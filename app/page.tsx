c3ac093d58a9bf5a7ccf4e582c664f216c83cf4d
Samuel Pokam <dixiades@gmail.com>
2023-06-06 22:00:41 +0100

    schema update

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <div>Hello</div>
+import { getTodos } from "@/lib/todos"
+
+export default async function Home() {
+  const { todos } = await getTodos()
+  return (
+    <section className="py-20">
+      <div className="container">
+        <h1>Todos</h1>
+        <h2>Previous todos:</h2>
+        <ul>
+          {todos?.map((todo) => (
+            <li key={todo.id}>{todo.title}</li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  )
 }
